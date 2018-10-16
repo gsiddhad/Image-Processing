3daa172b12e83519c8a59792247f5a37eeb2bac0
Gourav Siddhad <gourav1607@gmail.com>
2018-10-16 12:13:54 +0530

    Updated ImagePad

--- Spatial_Filter.m
+++ Spatial_Filter.m
@@ -7,15 +7,18 @@ function Spatial_Filter()
     GRAY = rgb2gray(RGB);
     
     % Adding Noise to Image
-    GRAYSP = imnoise(GRAY, 'salt & pepper', 0.05);
+    GRAYS = imnoise(GRAY, 'salt & pepper', 0.03);
 
     % Median Filter Mask
-    FILTER = (1/16)*[ 1 2 1;
-                      2 4 2;
+    FILTER = (1/16)*[ 1 2 1 ;
+                      2 4 2 ;
                       1 2 1 ];
                   
+    % Padding Image With Zeros
+    % GRAYSP = imgPad(GRAYS, 1);
+    
     % Applying Filter Multiple Times
-    IMG1 = imgFilter(GRAYSP, FILTER);
+    IMG1 = imgFilter(GRAYS, FILTER);
     IMG2 = imgFilter(IMG1, FILTER);
     IMG3 = imgFilter(IMG2, FILTER);
     IMG4 = imgFilter(IMG3, FILTER);
@@ -25,7 +28,7 @@ function Spatial_Filter()
     imshow(GRAY);
     title('Original');
     subplot(2,3,2);
-    imshow(GRAYSP);
+    imshow(GRAYS);
     title('Salt & Pepper');
     subplot(2,3,3);
     imshow(IMG1);
@@ -51,6 +54,17 @@ function IMG = imgFilter(ORIG, FILTER)
             IMG(i,j) = (ORIG(i-1,j-1)*FILTER(1,1) + ORIG(i-1,j)*FILTER(1,2) + ORIG(i-1,j+1)*FILTER(1,3) ...
                       + ORIG(i,j-1)*FILTER(2,1) + ORIG(i,j)*FILTER(2,2) + ORIG(i,j+1)*FILTER(2,3) ...
                       + ORIG(i+1,j-1)*FILTER(3,1) + ORIG(i+1,j)*FILTER(3,2) + ORIG(i+1,j+1)*FILTER(3,3));
+        end
+    end    
+end
+
+% Function to Pad Image with Zeros
+function IMG = imgPad(ORIG, PAD)
+    [p,q] = size(ORIG);
+    IMG = zeros(p+2, q+2);
+    for i=1:p
+        for j=1:q
+            IMG(i+PAD,j+PAD) = ORIG(i,j);
         end
     end
 end
